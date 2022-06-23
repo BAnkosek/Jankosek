deadfddf137a5b5be25cd0dd28b25c89b08d5a43
BAnkosek <108076823+BAnkosek@users.noreply.github.com>
2022-06-23 16:07:40 +0200

    Update copypasta.py

--- copypasta.py
+++ copypasta.py
@@ -1,3 +1,7 @@
+## this code inserts two | characters before and after every character of whatever text you put into "copy" variable,
+## this makes every character its own individual spoiler on discord, and makes it a pain to read any message
+## there are however relatively simple workarounds to this ;p 
+
 copy = list("")
 i = 0
 while True:
